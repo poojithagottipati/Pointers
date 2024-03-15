5015638761cc137e5dfaa8a6e3b7b7063f5b93ac
poojithagottipati <160849305+poojithagottipati@users.noreply.github.com>
2024-03-15 16:29:12 +0530

    Create LengthofString.c

---
+++ LengthofString.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    char *ptr = str;
+
+    int length = 0;
+
+    while (*ptr != 0) {
+        length++; 
+        ptr++;   
+    }
+
+    printf("Length of the string: %d\n", length);
+
+    return 0;
+}
+/*
+Enter a string: poojitha
+Length of the string: 8
+*/
